34552d06338e63065a9b44dc64cfa91cbb9f99a0
fidzzcodex.me <rinatonidi28@gmail.com>
2026-09-19 12:36:46 +0700

    Upload: add app/src/main/java/com/fidzz/dnsswitch/ui/theme/Color.kt

---
+++ app/src/main/java/com/fidzz/dnsswitch/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.fidzz.dnsswitch.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Ink = Color(0xFF0F2247)
+val Cloud = Color(0xFFF4F8FF)
+val Mist = Color(0xFFE8F0FE)
+val Line = Color(0xFFDBE7FB)
+val BrandBlue = Color(0xFF356DFB)
+val BrandBlueDark = Color(0xFF204DE0)
+val Ok = Color(0xFF16A34A)
+val Danger = Color(0xFFDC2626)
